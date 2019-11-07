1e8df0d8ce0cabb96657ca5d4f746c84d22c08d9
João Vitor Duarte Mariucio <joao.duarte@ftdata.com.br>
2019-11-07 16:40:50 -0300

    Initial Commit

---
+++ ExemploMQTT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ExemploMQTT
+//
+//  Created by João Vitor Duarte Mariucio on 07/11/19.
+//  Copyright © 2019 João Duarte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ExemploMQTTTests/ExemploMQTTTests.swift
@@ -0,0 +1,34 @@
+//
+//  ExemploMQTTTests.swift
+//  ExemploMQTTTests
+//
+//  Created by João Vitor Duarte Mariucio on 07/11/19.
+//  Copyright © 2019 João Duarte. All rights reserved.
+//
+
+import XCTest
+@testable import ExemploMQTT
+
+class ExemploMQTTTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
